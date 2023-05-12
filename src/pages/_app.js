c1be9cd547eebe7a77e9da84fc0625387788ae75
himakhaitan <himanshukhaitan108@gmail.com>
2023-05-13 02:26:45 +0530

    Updated Warning - No stylesheet in Head

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,16 +5,6 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
         <title>Cockroach Poker</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
