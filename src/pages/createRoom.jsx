4f4ff927b2e8147671801399eed26f19d5bc2f00
himakhaitan <himanshukhaitan108@gmail.com>
2023-05-13 20:32:09 +0530

    Create Room Added

---
+++ src/pages/createRoom.jsx
@@ -0,0 +1,60 @@
+import classes from "../styles/createRoom.module.css";
+import Navigation from "@/components/Navigation";
+import Link from "next/link";
+const CreateRoom = ({}) => {
+  return (
+    <div
+      className={`h-screen bg-gradient-to-tr from-black via-slate-800 to-red-900 ${classes.createRoom}`}
+    >
+      <Navigation />
+      <div className="grid grid-cols-2 gap-8 px-40 py-60 items-center">
+        <div>
+          <h2 className="bg-gradient-to-r from-neutral-50 to-blue-500 bg-clip-text text-transparent text-8xl font-bold">
+            Let's Start.
+          </h2>
+          <h4 className="text-white text-4xl mt-20 font-medium">
+            Except that the game is all about bluffing!
+          </h4>
+          <Link href="/" className="inline-flex items-stretch">
+            <button className="bg-transparent hover:bg-white text-gray-100 border border-white font-bold mt-9 py-5 px-4 rounded inline-flex items-center">
+              <svg
+                className="fill-white w-8 h-8 mr-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <path d="M48.5 224H40c-13.3 0-24-10.7-24-24V72c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2L98.6 96.6c87.6-86.5 228.7-86.2 315.8 1c87.5 87.5 87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3c-62.2-62.2-162.7-62.5-225.3-1L185 183c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8H48.5z" />
+              </svg>
+              <span className=" font-normal ">Return to Home</span>
+            </button>
+          </Link>
+        </div>
+        <div>
+          <form class="w-full max-w-4xl">
+            <div class="flex items-center border-b border-white py-3">
+              <input
+                class="appearance-none bg-transparent border-none w-full text-gray-200 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                type="text"
+                placeholder="Enter Room Name"
+                aria-label="Full name"
+              />
+              <button
+                class="flex-shrink-0 bg-red-500 hover:bg-red-700 border-red-500 hover:border-red-700 text-2xl border-4 text-white py-3 px-6 rounded"
+                type="button"
+              >
+                Create Room
+              </button>
+              <button
+                class="flex-shrink-0 border-transparent border-4 text-gray-100 hover:text-gray-300 text-2xl py-1 px-2 rounded"
+                type="button"
+              >
+                Random
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateRoom;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,33 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { selectUserName } from "../store/slices/userSlice";
 import Navigation from "../components/Navigation";
 import classes from "../styles/home.module.css";
 import randomNumber from "@/utils/randomNumber";
+import Link from "next/link";
 
 export default function Home() {
-
   const dispatch = useDispatch();
+  const nameFetchedRef = useRef(false);
+  const userName = useSelector(selectUserName);
 
   useEffect(() => {
     // Generating Random Name and Avatar
-    dispatch({type: 'user/setName', payload: randomNumber(0, 52)});
-    dispatch({type: 'user/setAvatar', payload: randomNumber(1, 9)});
+    if (nameFetchedRef.current) return;
 
+    if (userName == "unknown") {
+      dispatch({ type: "user/setName", payload: randomNumber(0, 52) });
+      dispatch({ type: "user/setAvatar", payload: randomNumber(1, 9) });
+      nameFetchedRef.current = true;
+    }
   }, []);
 
   return (
     <div
       className={`h-screen bg-gradient-to-tr from-black via-slate-800 to-red-900 ${classes.home}`}
     >
-      <Navigation/>
+      <Navigation />
       <div className="grid grid-cols-2 gap-8 px-40 py-60 items-center">
         <div className="">
           <h2 className="bg-gradient-to-r from-neutral-50 to-yellow-500 bg-clip-text text-transparent text-8xl font-bold">
@@ -33,26 +41,30 @@ export default function Home() {
           </h4>
 
           <div className="mt-20 flex  gap-5">
-            <button className="bg-red-500 hover:bg-red-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-              <svg
-                className="fill-white w-8 h-8 mr-2"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
-              >
-                <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-              </svg>
-              <span>Start a Game</span>
-            </button>
-            <button className="bg-transparent hover:bg-white-500 text-white-700 font-semibold text-white py-2 px-6 border hover:border-white border-transparent rounded">
-              <svg
-                className=" fill-red-500 w-8 h-8 mr-2"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 384 512"
-              >
-                <path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />
-              </svg>
-              <span>Join a Game</span>
-            </button>
+            <Link href="/createRoom" className="inline-flex items-stretch">
+              <button className="bg-red-500 hover:bg-red-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+                <svg
+                  className="fill-white w-8 h-8 mr-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                >
+                  <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                </svg>
+                <span>Start a Game</span>
+              </button>
+            </Link>
+            <Link href="/createRoom" className="inline-flex items-stretch">
+              <button className="bg-transparent hover:bg-white-500 text-white-700 font-semibold text-white py-2 px-6 border hover:border-white border-transparent rounded">
+                <svg
+                  className=" fill-red-500 w-8 h-8 mr-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 384 512"
+                >
+                  <path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />
+                </svg>
+                <span>Join a Game</span>
+              </button>
+            </Link>
           </div>
         </div>
         <div className="">
