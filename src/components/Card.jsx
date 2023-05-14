3c44dd41fa8389084fac03fc424dc4fe959c1ba9
himakhaitan <himanshukhaitan108@gmail.com>
2023-05-15 02:14:18 +0530

    Dasboard

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import { cardImages } from "@/game/card";
 
 const Card = ({ type }) => {
   return (
-    <div className="p-5 bg-white rounded-xl transition ease-in-out hover:scale-110	">
+    <div className=" p-2 bg-white rounded-xl transition ease-in-out hover:scale-110	">
       <img src={"/cards/" + cardImages[type]} />
     </div>
   );
--- src/pages/game/game.js
+++ src/pages/game/game.js
@@ -34,24 +34,100 @@ const Game = ({}) => {
           <Card type="RAT" />
           <Card type="BEE" />
         </div>
-        <div className="mt-10 grid grid-cols-3 gap-8 px-40 py-20">
+        <div className="mt-10 grid grid-cols-3 gap-20 px-40 py-20">
           <div>
             <h1 className="text-white text-left font-semibold text-4xl">
               Players
             </h1>
             <div className="grid grid-cols-1 mt-10">
-              <div className="rounded-lg flex bg-opacity-70 place-content-between items-center px-7 py-5 bg-zinc-800 mb-5">
-                <img className="h-20 w-20 rounded-full" src="/heads/1.png" />
-                <p className="text-gray-200 font-medium">Himanshu</p>
+              <div className="rounded-lg  bg-opacity-70  px-7 py-5 bg-zinc-800 mb-5">
+                <div className="flex place-content-between items-center border-b-2 border-yellow-700 pb-5">
+                  <img className="h-20 w-20 rounded-full" src="/heads/1.png" />
+                  <p className="text-gray-200 font-medium">Himanshu</p>
+                </div>
+                <div className="grid grid-cols-8 gap-2 mt-4">
+                  <Card type="RAT" />
+                  <Card type="SCORPION" />
+                  <Card type="BUG" />
+                  <Card type="TORD" />
+                  <Card type="RAT" />
+                  <Card type="SCORPION" />
+                  <Card type="COCKROACH" />
+                  <Card type="BEE" />
+                  <Card type="SCORPION" />
+                  <Card type="COCKROACH" />
+                  <Card type="BEE" />
+                  <Card type="SCORPION" />
+                  <Card type="BUG" />
+                  <Card type="TORD" />
+                  <Card type="RAT" />
+                  <Card type="BEE" />
+                </div>
               </div>
-              <div className="rounded-lg flex bg-opacity-70 place-content-between items-center px-7 py-5 bg-zinc-800 mb-5">
-                <img className="h-20 w-20 rounded-full" src="/heads/1.png" />
-                <p className="text-gray-200 font-medium">Himanshu</p>
+              <div className="rounded-lg  bg-opacity-70  px-7 py-5 bg-zinc-800 mb-5">
+                <div className="flex place-content-between items-center border-b-2 border-yellow-700 pb-5">
+                  <img className="h-20 w-20 rounded-full" src="/heads/1.png" />
+                  <p className="text-gray-200 font-medium">Himanshu</p>
+                </div>
+                <div className="grid grid-cols-8 gap-2 mt-4">
+                  <Card type="RAT" />
+                  <Card type="SCORPION" />
+                  <Card type="TORD" />
+                  <Card type="RAT" />
+                  <Card type="BEE" />
+                </div>
               </div>
-              <div className="rounded-lg flex bg-opacity-70 place-content-between items-center px-7 py-5 bg-zinc-800 mb-5">
-                <img className="h-20 w-20 rounded-full" src="/heads/1.png" />
-                <p className="text-gray-200 font-medium">Himanshu</p>
+              <div className="rounded-lg  bg-opacity-70  px-7 py-5 bg-zinc-800 mb-5">
+                <div className="flex place-content-between items-center border-b-2 border-yellow-700 pb-5">
+                  <img className="h-20 w-20 rounded-full" src="/heads/1.png" />
+                  <p className="text-gray-200 font-medium">Himanshu</p>
+                </div>
+                <div className="grid grid-cols-8 gap-2 mt-4">
+                  <Card type="RAT" />
+                  <Card type="SCORPION" />
+                  <Card type="BUG" />
+                  <Card type="TORD" />
+                  <Card type="RAT" />
+                  <Card type="SCORPION" />
+                  <Card type="COCKROACH" />
+                  <Card type="BEE" />
+                  <Card type="SCORPION" />
+                
+                  <Card type="TORD" />
+                  <Card type="RAT" />
+                  <Card type="BEE" />
+                </div>
               </div>
+            </div>
+          </div>
+          <div>
+            <h1 className="text-white text-left font-semibold text-4xl">
+              Actions
+            </h1>
+          </div>
+          <div>
+            <h1 className="text-white text-left font-semibold text-4xl mb-10">
+              Game Logs
+            </h1>
+            <div className="h-96 rounded-2xl overflow-auto">
+              <p className="text-zinc-800 font-medium bg-gray-200 py-2 px-4 mb-4 bg-opacity-80 rounded">
+                - Raj played the card to you.
+              </p>
+              <p className="text-zinc-800 font-medium bg-gray-200 py-2 px-4 mb-4 bg-opacity-80 rounded">
+                - Raj played the card to you.
+              </p>
+              <p className="text-zinc-800 font-medium bg-gray-200 py-2 px-4 mb-4 bg-opacity-80 rounded">
+                - Raj played the card to you.
+              </p>
+              <p className="text-zinc-800 font-medium bg-gray-200 py-2 px-4 mb-4 bg-opacity-80 rounded">
+                - Raj played the card to you.
+              </p>
+              <p className="text-zinc-800 font-medium bg-gray-200 py-2 px-4 mb-4 bg-opacity-80 rounded">
+                - Raj played the card to you.
+              </p>
+              <p className="text-zinc-800 font-medium bg-gray-200 py-2 px-4 mb-4 bg-opacity-80 rounded">
+                - Raj played the card to you.
+              </p>
             </div>
           </div>
         </div>
