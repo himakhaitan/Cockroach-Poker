f03b710c5202a0f108dd9ecbaa72d111f6324688
himakhaitan <himanshukhaitan108@gmail.com>
2023-05-16 19:39:09 +0530

    Set turn init

--- src/pages/game/[game].js
+++ src/pages/game/[game].js
@@ -8,26 +8,38 @@ import GetPlayed from "@/components/Form/GetPlayed";
 import Card from "@/components/Card";
 import SOCKET_EVENTS from "@/utils/socketEvents";
 
-
 import { useRouter } from "next/router";
-
+import { selectUserName } from "@/store/slices/userSlice";
 const Game = ({}) => {
   const [isPlayed, setIsPlayed] = useState(true);
   const [cards, setCards] = useState([]);
+  const [roomName, setRoomName] = useState("");
+  const [players, setPlayers] = useState([]);
   const router = useRouter();
+  const name = useSelector(selectUserName);
   const socket = useSelector(selectSocket);
   const { game } = router.query;
 
   useEffect(() => {
-
-    socket.emit(SOCKET_EVENTS.LOAD_CARDS_REQUEST, game);
+    socket.emit(SOCKET_EVENTS.LOAD_CARDS_REQUEST, {
+      roomId: game,
+      name: name,
+    });
 
     socket.on(SOCKET_EVENTS.LOAD_CARDS, (data) => {
       console.log(data);
       setCards(data);
-    })
+    });
 
-  }, [])
+    socket.on(SOCKET_EVENTS.UPDATE_BOARD, (data) => {
+      setRoomName(data.roomName);
+      setPlayers(data.players);
+    });
+
+    socket.on(SOCKET_EVENTS.SET_TURN, (data) => {
+      setIsPlayed(!data);
+    });
+  }, []);
 
   return (
     <div
@@ -36,7 +48,7 @@ const Game = ({}) => {
       <Navigation />
       <div className="w-full">
         <h1 className="bg-gradient-to-r from-neutral-50 to-yellow-500 bg-clip-text text-transparent text-center font-bold text-6xl mb-10">
-          Game Name
+          {roomName}
         </h1>
         <h1 className="text-white text-left px-40 font-semibold text-4xl">
           Your Cards
@@ -52,45 +64,22 @@ const Game = ({}) => {
               Players
             </h1>
             <div className="grid grid-cols-1 mt-10">
-              <div className="rounded-lg  bg-opacity-70  px-7 py-5 bg-zinc-800 mb-5">
-                <div className="flex place-content-between items-center border-b-2 border-yellow-700 pb-5">
-                  <img className="h-20 w-20 rounded-full" src="/heads/1.png" />
-                  <p className="text-gray-200 font-medium">Himanshu</p>
-                </div>
-                <div className="grid grid-cols-8 gap-2 mt-4">
-                  <Card type="RAT" />
-                  <Card type="SCORPION" />
-                  <Card type="BUG" />
-                  <Card type="TORD" />
-                  <Card type="RAT" />
-                  <Card type="SCORPION" />
-                </div>
-              </div>
-              <div className="rounded-lg  bg-opacity-70  px-7 py-5 bg-zinc-800 mb-5">
-                <div className="flex place-content-between items-center border-b-2 border-yellow-700 pb-5">
-                  <img className="h-20 w-20 rounded-full" src="/heads/1.png" />
-                  <p className="text-gray-200 font-medium">Himanshu</p>
-                </div>
-                <div className="grid grid-cols-8 gap-2 mt-4">
-                  <Card type="RAT" />
-                  <Card type="SCORPION" />
-                  <Card type="TORD" />
-                  <Card type="RAT" />
-                  <Card type="BEE" />
-                </div>
-              </div>
-              <div className="rounded-lg  bg-opacity-70  px-7 py-5 bg-zinc-800 mb-5">
-                <div className="flex place-content-between items-center border-b-2 border-yellow-700 pb-5">
-                  <img className="h-20 w-20 rounded-full" src="/heads/1.png" />
-                  <p className="text-gray-200 font-medium">Himanshu</p>
-                </div>
-                <div className="grid grid-cols-8 gap-2 mt-4">
-                  <Card type="RAT" />
-                  <Card type="SCORPION" />
-                  <Card type="BUG" />
-                  <Card type="TORD" />
-                </div>
-              </div>
+              {players.map((player) => {
+                return (
+                  <div className="rounded-lg  bg-opacity-70  px-7 py-5 bg-zinc-800 mb-5">
+                    <div className="flex place-content-between items-center border-b-2 border-yellow-700 pb-5">
+                      <img
+                        className="h-20 w-20 rounded-full"
+                        src={`/heads/${player.avatar}.png`}
+                      />
+                      <p className="text-gray-200 font-medium">{player.name}</p>
+                    </div>
+                    {player.lost.map((card) => {
+                      return <Card type={card} />;
+                    })}
+                  </div>
+                );
+              })}
             </div>
           </div>
           <div>
@@ -110,7 +99,6 @@ const Game = ({}) => {
               <p className="text-zinc-800 font-medium bg-gray-200 py-2 px-4 mb-4 bg-opacity-80 rounded">
                 - Raj played the card to you.
               </p>
-             
             </div>
           </div>
         </div>
